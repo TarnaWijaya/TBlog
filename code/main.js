533291a8c8c77b9a565bf1affeed19045df5bc7e
Tarna <158784522+TarnaWijaya@users.noreply.github.com>
2024-12-17 00:58:50 +0700

    update code/main.js

--- code/main.js
+++ code/main.js
@@ -29,7 +29,9 @@ async function sendMessage() {
     try {
         // Fetch data dari Gemini API
         const apiKey = "AIzaSyC0Cjd5U_kIM9tvqxfjjvQ_MlhabjtxA30";
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`, {
+        const response = await
+        fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash-latest:generateContent?key=${apiKey}`,
+        {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
